fb7c5f32fdcc41e9064d59f0b4e450c65755a7e2
Bent Bracke <bent@bracke.dk>
2021-08-04 18:24:26 +0200

    Make navbar interactive/work

---
+++ app/components/navbar.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class NavbarComponent extends Component {
+
+  show;
+
+  constructor() {
+    super(...arguments);
+
+    this.show = false;
+  }
+}
