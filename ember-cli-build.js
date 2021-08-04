98a7d9d1b819ad21f02ea041ab8008ef4d9f0754
Bent Bracke <bent@bracke.dk>
2021-08-04 16:40:29 +0200

    Optimized service worker settings

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-
+    'ember-service-worker': {
+      registrationStrategy: 'inline',
+      versionStrategy: 'every-build'
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
