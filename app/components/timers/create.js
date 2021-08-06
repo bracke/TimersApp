3f91098d197b0f8f48df8412c560f836262240a9
Bent Bracke <bent@bracke.dk>
2021-08-06 11:50:38 +0200

    Add validation to create timer

--- app/components/timers/create.js
+++ app/components/timers/create.js
@@ -8,11 +8,17 @@ export default class TimersCreateComponent extends Component {
   @tracked New_Timer_Is_Countdown;
   @tracked New_Timer_Play_Sound_When_Done;
 
+  @tracked New_Timer_Name_IsValid;
+  @tracked New_Timer_Target_Runtime_In_Minutes_IsValid;
+
   initialize_new_timer() {
     this.New_Timer_Name = 'Untitled';
     this.New_Timer_Target_Runtime_In_Minutes = 0;
     this.New_Timer_Is_Countdown = false;
     this.New_Timer_Play_Sound_When_Done = false;
+
+    this.New_Timer_Name_IsValid = true;
+    this.New_Timer_Target_Runtime_In_Minutes_IsValid = true;
   }
   constructor() {
     super(...arguments);
@@ -26,15 +32,40 @@ export default class TimersCreateComponent extends Component {
     this.New_Timer_Is_Countdown = Is_Countdown;
     this.New_Timer_Play_Sound_When_Done = Play_Sound_When_Done;
   }
+  validate() {
+    let isValid = true;
+    const Minutes_In_A_Day = 24 * 60;
+
+    if (this.New_Timer_Name === '') {
+      isValid = false;
+      this.New_Timer_Name_IsValid = false;
+    } else {
+      this.New_Timer_Name_IsValid = true;
+    }
+    if (
+      this.New_Timer_Is_Countdown &&
+      (this.New_Timer_Target_Runtime_In_Minutes === 0 ||
+        this.New_Timer_Target_Runtime_In_Minutes > Minutes_In_A_Day)
+    ) {
+      isValid = false;
+      this.New_Timer_Target_Runtime_In_Minutes_IsValid = false;
+    } else {
+      this.New_Timer_Target_Runtime_In_Minutes_IsValid = true;
+    }
+    return isValid;
+  }
   @action
   addTimer() {
-    // TODO: Validate New_Timer before creation
-    this.args.addTimer(
-      this.New_Timer_Name,
-      this.New_Timer_Target_Runtime_In_Minutes,
-      this.New_Timer_Is_Countdown,
-      this.New_Timer_Play_Sound_When_Done
-    );
-    this.initialize_new_timer();
+    let isValid = this.validate();
+
+    if (isValid) {
+      this.args.addTimer(
+        this.New_Timer_Name,
+        this.New_Timer_Target_Runtime_In_Minutes,
+        this.New_Timer_Is_Countdown,
+        this.New_Timer_Play_Sound_When_Done
+      );
+      this.initialize_new_timer();
+    }
   }
 }
