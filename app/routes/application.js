31fd967c36c4d0de2b794eb031efdee58c81907e
Bent Bracke <bent@bracke.dk>
2021-08-07 12:03:25 +0200

    Added translations

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,11 +2,18 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
+
   @service intl;
+  @service('best-language') bestLanguage;
 
   beforeModel() {
     this._super(...arguments);
 
-    this.intl.setLocale(['en-us']);
+    const bestLanguage = this.bestLanguage.bestLanguageOrFirst([
+      'en-US',
+      'de-DE',
+      'da-DK',
+    ]);
+    this.intl.setLocale([bestLanguage.language]);
   }
 }
