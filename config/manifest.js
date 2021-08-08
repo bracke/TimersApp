47ca15ca3177fa72104c753490d18c394db3a4fb
Bent Bracke <bent@bracke.dk>
2021-08-08 20:56:51 +0200

    Added image links in manifest

--- config/manifest.js
+++ config/manifest.js
@@ -13,7 +13,23 @@ module.exports = function (/* environment, appConfig */) {
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
-    icons: [],
+    icons: [
+      {
+        src: 'favicon.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        src: 'apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
+        src: 'icon192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
     ms: {
       tileColor: '#fff',
     },
